e47a2f57c396cf90136cd27ffdd682d19590b40a
Zachary Thompson <zthompson@myseneca.ca>
2024-09-27 01:05:46 -0400

    Added complex arithmetic operations to lab1d script

--- lab1d.py
+++ lab1d.py
@@ -4,6 +4,14 @@ x = 10
 y = 2
 z = 5
 
-result = x + y * z
+result1 = x + y * z
+result2 = (x + y) * z
+result3 = x + y * z - x ** 2
+result4 = 15 / 3 * 4
+result5 = 100 / ((y + y) * z)
 
-print(f'{x} + {y} * {z} = {result}')
+print(f'{x} + {y} * {z} = {result1}')
+print(f'({x} + {y}) * {z} = {result2}')
+print(f'{x} + {y} * {z} - {x} ** 2 = {result3}')
+print(f'15 / 3 * 4 = {result4}')
+print(f'100 / (({y} + {y}) * {z}) = {result5}')
